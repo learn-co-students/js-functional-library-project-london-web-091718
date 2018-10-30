bd019aa48658c7e02f1fe8732de33756eaec5d17
Ryan <ryangeorgebarker0@gmail.com>
2018-10-30 15:31:13 +0000

    Done.

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(array, callback) {
 
+      for (const element in array) {
+        callback(array[element]);
+      }
+      return array
     },
 
-    map: function() {
+    map: function(array, callback) {
+      const newArray = []
 
+      for (const element of array) {
+        newArray.push(callback(element))
+      }
+      return newArray
     },
 
     reduce: function() {
 
     },
-    
+
     functions: function() {
 
     },
